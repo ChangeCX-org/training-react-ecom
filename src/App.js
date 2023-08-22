f19327ddb5e8ec1b5819f6503dd82130b1238e13
Pankhuri <pankhuri.garg@changecx.com>
2023-08-22 16:49:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="p-10">
+    <div className="px-10">
       <Header/>
       <HomePageBanner></HomePageBanner>
       <CurratedBlock/>
